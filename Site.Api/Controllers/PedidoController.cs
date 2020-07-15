1220e17061abdbadb1f33f51064cea2e3ed49050
Marcos Sabino <marcos.sabino@outlook.com>
2020-07-14 21:15:30 -0300

    Adicionar arquivos de projeto.

---
+++ Site.Api/Controllers/PedidoController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace Site.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PedidoController : ControllerBase
+    {
+		private ILogger<PedidoController> _log;
+
+		public PedidoController(ILogger<PedidoController> log)
+		{
+			_log = log;
+		}
+
+		[HttpPost("gerarPedido")]
+		public IActionResult Gerar(Domain.Pedido pedido)
+		{
+			try
+			{
+				#region [ Fila ]
+
+				var factory = new ConnectionFactory() { HostName = "localhost" };
+				using (var connection = factory.CreateConnection())
+				using (var channel = connection.CreateModel())
+				{
+					channel.QueueDeclare(queue: "filaPedidos",
+										 durable: false,
+										 exclusive: false,
+										 autoDelete: false,
+										 arguments: null);
+
+					string objetoSerializado = JsonConvert.SerializeObject(pedido);
+					var body = Encoding.UTF8.GetBytes(objetoSerializado);
+
+					channel.BasicPublish(exchange: "",
+										 routingKey: "filaPedidos",
+										 basicProperties: null,
+										 body: body);
+					//Console.WriteLine(" [x] Sent {0}", message);
+				}
+
+				#endregion
+
+				return Accepted(pedido);
+			}
+			catch (Exception ex)
+			{
+				_log.LogError(ex.Message, ex);
+				return BadRequest(ex.Message);
+			}
+
+		}
+    }
+}
---
+++ Site.Api/Domain/Pedido.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Site.Api.Domain
+{
+	public class Pedido
+	{
+		public int IdPedido { get; set; }
+		public int Quantidade { get; set; }
+		public int IdProduto { get; set; }
+		public int Valor { get; set; }
+	}
+}
